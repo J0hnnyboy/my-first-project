33d6391a25cfc1cde6be0315b49be8acb94bafed
J0hnnyboy <101340085+J0hnnyboy@users.noreply.github.com>
2022-03-10 09:24:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Luca Sucks at Clash")
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+	
+})
